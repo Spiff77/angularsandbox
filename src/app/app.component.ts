5bf0720d47e45c0d2720eac0965ecb7080d9f25c
Thomas Lhomme <blabla@blabla.com>
2021-04-29 15:16:21 +0200

    click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,18 @@ import {Supplier} from './model/supplier.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  count = 0;
+
   client = new Client(10, 'Jean', 'Jacques', true, 20);
   product = new Product(10, 'Jean', 'Vetements', 'C\'est cool', 15,  true, 10);
   supplier = new Supplier(10, 'Pierre', 'Paul', 'Google', '123456789', 200000, true, 25);
+
+  increment(): void {
+    this.count++;
+  }
+
+  toggle(): void {
+    this.client.active = !this.client.active;
+  }
 }
