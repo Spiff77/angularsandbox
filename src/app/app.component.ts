e86ae6451f1ffa060178246e32bcd921e3cb3cbf
Thomas Lhomme <blabla@blabla.com>
2021-04-29 14:22:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Client} from './model/client.model';
+import {Product} from './model/product.model';
+import {Supplier} from './model/supplier.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sandbox';
+  client = new Client(10, 'Jean', 'Jacques', true, 20);
+  product = new Product(10, 'Jean', 'Vetements', 'C\'est cool', 15,  true, 10);
+  supplier = new Supplier(10, 'Pierre', 'Paul', 'Google', '123456789', 200000, true, 25);
 }
